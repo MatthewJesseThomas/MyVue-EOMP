e30a038258db65ef33d470013d43daef21ac6fa6
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-01-12 11:42:02 +0200

    'CreatedCardsandAddedProps'

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="col-11 col-sm-4 card bg-dark mx-3 my-3" style="width: 18rem;">
-        <img src="https://i.postimg.cc/kX8XT5g3/profile-1.jpg" class="card-img-top" alt="Meh" loading="lazy">
+        <img :src="imgSrc" class="card-img-top" alt="Meh" loading="lazy">
         <div class="card-body">
-          <h6 class="display-6 card-title kage animate-charcter fw-bold">Camryn Herandien <br>Colleague</h6>
-          <p class="card-text kage animate-charcter fw-semibold">Matthew is well behaved, understanding and a Respectful and Dilligent colleague, I can atest to his ability on working on Front-End and Back-End projects. His an Asset at any Level and Awesome to work alongside!</p>
+          <h6 class="display-6 card-title kage animate-charcter fw-bold"> {{ name }} <br>{{ relation }} </h6>
+          <p class="card-text kage animate-charcter fw-semibold">{{ quote }} </p>
         </div>
     </div>
 </template>
 <script>
 export default {
-    
+    props: ['imgSrc', 'name', 'relation', 'quote']
 }
 </script>
 <style>
