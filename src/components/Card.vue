96914b18993c0944d5a8b409334102f2c867709e
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-01-12 10:04:55 +0200

    'ContactFormSubmission'

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="col-11 col-sm-4 card bg-dark mx-3 my-3" style="width: 18rem;">
+        <img src="https://i.postimg.cc/kX8XT5g3/profile-1.jpg" class="card-img-top" alt="Meh" loading="lazy">
+        <div class="card-body">
+          <h6 class="display-6 card-title kage animate-charcter fw-bold">Camryn Herandien <br>Colleague</h6>
+          <p class="card-text kage animate-charcter fw-semibold">Matthew is well behaved, understanding and a Respectful and Dilligent colleague, I can atest to his ability on working on Front-End and Back-End projects. His an Asset at any Level and Awesome to work alongside!</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
