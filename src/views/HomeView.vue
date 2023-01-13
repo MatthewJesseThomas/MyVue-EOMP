7b4f3dd7a42e7e126215313a5e92ff35e4642b1f
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-01-13 10:52:53 +0200

    'ImagesRounded'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,22 +9,30 @@
           <figure>
               <div class=" my-auto">
                   <div class="img-fluids d-flex alert-danger flex-column flex-sm-row">
-                      <img class="img-fluid my-3" data-aos="fade-right"
+                      <img class="img-fluid my-3 image-round" data-aos="fade-right"
                           src="https://i.postimg.cc/xTXJBTtq/Profile-Pic1.jpg"
                           alt="Matthew profile" loading="lazy">
-                      <img class="img-fluid my-3" data-aos="fade-left"
-                          src="https://i.postimg.cc/TYb2rytj/Profile-Pic2.jpg" alt="Matthew profile" loading="lazy">
+                        <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible border-5 w-100 h-100 mt-5" data-aos="flip-down">
+                            <p class="text-light align-self-center justify-content-md-around fw-bolder">
+                                Hello There I am Matthew Thomas,
+                                I'm inspired by the work I've done in Software Development in all its success, and not to mention my unique career path Choice.
+                                I saw a recent article on managing Developers in the Software Industry and had to Begin Now. I've managed to  accelerate my Development in Web and Software Development, which has contributed toward as much as 90% growth in as few as three months.</p> 
+                        </div>
                   </div>
                   <div class="img-fluids d-flex alert-danger flex-column flex-sm-row">
-                      <img class="img-fluid my-3 resize" data-aos="fade-right"
+                      <img class="img-fluid my-3 resize image-rounded" data-aos="fade-right"
                           src="https://i.postimg.cc/VkDNNx72/Profile-Pic4.jpg"
                           alt="Matthew profile" loading="lazy">
-                      <img class="img-fluid my-3 resize" data-aos="fade-left"
-                          src="https://i.postimg.cc/1XQSBBCp/Profile-Pic5.jpg" alt="Matthew profile" loading="lazy">
+                          <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible border-5 w-100 h-100 mt-5" data-aos="flip-down">
+                            <p class="text-light align-self-center justify-content-md-around fw-bolder">
+                                Matthew Thomas,
+                                Its inspiring to be in the working industry of Software Development. Success in a Software Development career is Somewhat of a Dream for me.
+                                As a Aspiring Developer in the Industry it Begins Now. I've managed to  Develop a 'Critical' way of thinking and handling projects in Web and Software Development, which has contributed towards a much Confidence as well as Mindset with a focus towards "Exponential" growth in as few months.</p> 
+                        </div>
                   </div>
               </div>
               <figcaption class="animated-charcter fw-semibold">
-                  <p><strong>Aspiring Developer | Aspiring Web Designer | Aspiring Coder</strong></p>
+                  <p class="text-light"><strong>Aspiring Developer | Aspiring Web Designer | Aspiring Coder</strong></p>
               </figcaption>
           </figure>
       </div>
@@ -44,5 +52,15 @@ export default {
 }
 figcaption{
     background-color: rgb(36, 54, 54);
+}
+.image-round{
+    width: 600px;
+    height: 400px;
+    border-radius: 1500px;
+}
+.image-rounded{
+    width: 1000px;
+    height: 400px;
+    border-radius: 1500px;
 }
 </style>
