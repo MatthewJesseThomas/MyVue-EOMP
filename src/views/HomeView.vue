3b98835f7fac2131f96094b83d3be21e7647369f
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-01-14 10:40:05 +0200

    'MoreStyling'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,9 +14,9 @@
                           alt="Matthew profile" loading="lazy">
                         <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible border-5 w-100 h-100 mt-5" data-aos="flip-down">
                             <p class="text-light align-self-center justify-content-md-around fw-bolder">
-                                Hello There I am Matthew Thomas,
+                                Hello There, I am Matthew Jesse Thomas,
                                 I'm inspired by the work I've done in Software Development in all its success, and not to mention my unique career path Choice.
-                                I saw a recent article on managing Developers in the Software Industry and had to Begin Now. I've managed to  accelerate my Development in Web and Software Development, which has contributed toward as much as 90% growth in as few as three months.</p> 
+                                I saw a recent article on managing Developers in the Software Industry and had to Commence. I've managed to accelerate my Developmental Process in Web & Software Development, which has contributed toward as much as 90% growth in as few as three months time.</p> 
                         </div>
                   </div>
                   <div class="img-fluids d-flex alert-danger flex-column flex-sm-row">
@@ -25,9 +25,10 @@
                           alt="Matthew profile" loading="lazy">
                           <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible border-5 w-100 h-100 mt-5" data-aos="flip-down">
                             <p class="text-light align-self-center justify-content-md-around fw-bolder">
-                                Matthew Thomas,
-                                Its inspiring to be in the working industry of Software Development. Success in a Software Development career is Somewhat of a Dream for me.
-                                As a Aspiring Developer in the Industry it Begins Now. I've managed to  Develop a 'Critical' way of thinking and handling projects in Web and Software Development, which has contributed towards a much Confidence as well as Mindset with a focus towards "Exponential" growth in as few months.</p> 
+                                •Matthew Thomas•
+                                <br>
+                                Its Inspiring to be in the working Industry of Software Development. Success in a Software Development career is Somewhat of a Dream for mine.
+                                As an Aspiring Developer in the Industry it Begins Now with me. Thus I've managed to Develop a 'Critical' way of thinking and handling 'Problems' in Web & Software Development, which has contributed towards a much needed sense of Confidence as well as a Mindset shift with a focus towards "Exponential" growth in the Near Future.</p> 
                         </div>
                   </div>
               </div>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -6,23 +6,23 @@
         </div>
     </div>
             <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible w-75 m-auto justify-content-center">
-                <h1 class="display-5 text-decoration-underline kage animated-charcter fw-bolder">|EDUCATION|</h1> <br>
+                <h1 class="display-5 text-decoration-underline kage animated-charcter fw-bolder">EDUCATION</h1> <br>
                 <h1 class="display-5 text-decoration-underline kage animate-charcter fw-bolder">Matriculated From:</h1> <br>
-                <h2 class="display-6 text-decoration-underline kage animate-charcter fw-bold">{{ resume[0].school }} </h2> <br>
-                <p class="lead display-6 kage animate-charcter fw-semibold">Year Of Matriculation:{{ resume[0].matriculation }} </p> <br>
-                <p class="lead display-6 kage animate-charcter fw-semibold">Matriculated With:Bachelors</p> <br>
+                <h2 class="display-6 kage animate-charcter fw-bold">{{ resume[0].school }} </h2> <br>
+                <p class="lead display-6 kage animate-charcter fw-semibold">Year Of Matriculation: <br> {{ resume[0].matriculation }} </p> <br>
+                <p class="lead display-6 kage animate-charcter fw-semibold">Matriculated With: <br> Bachelors</p> <br>
             </div>
             <br>
             <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible w-75 m-auto justify-content-center">
-                <h1 class="display-5 text-decoration-underline kage animated-charcter fw-bolder">|EDUCATION|</h1> <br>
-                <h1 class="display-5 text-decoration-underline kage animate-charcter fw-bolder">{{ resume[1].typeOfEducation }}</h1> <br>
-                <h2 class="display-6 text-decoration-underline kage animate-charcter fw-bold">{{resume[1].school}}</h2> <br>
+                <h1 class="display-5 text-decoration-underline kage animated-charcter fw-bolder">EDUCATION</h1> <br>
+                <h1 class="display-5 kage animate-charcter fw-bolder">{{ resume[1].typeOfEducation }}</h1> <br>
+                <h2 class="display-6  kage animate-charcter fw-bold">{{resume[1].school}}</h2> <br>
                 <p class="lead display-5 kage animate-charcter fw-semibold">{{resume[1 ].attended}}</p> <br>
             </div>
             <br>
             <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible w-75 m-auto justify-content-center">
                 <h1 class="display-5 text-decoration-underline kage animated-charcter fw-bolder">|EXPERIENCE:|</h1>
-                <h2 class="display-6 text-decoration-underline kage animate-charcter fw-bold">{{ resume[2].retail }} <br>{{ resume[2].when }} <br> {{ resume[2].fb }} <br> {{ resume[2].date }} <br> {{ resume[2].engineering }} <br> {{ resume[2].timeSpent }} <br> {{ resume[2].developer }} <br> {{ resume[2].timeFrame }} </h2>
+                <h2 class="display-6 kage animate-charcter fw-bold">{{ resume[2].retail }} <br>{{ resume[2].when }} <br> {{ resume[2].fb }} <br> {{ resume[2].date }} <br> {{ resume[2].engineering }} <br> {{ resume[2].timeSpent }} <br> {{ resume[2].developer }} <br> {{ resume[2].timeFrame }} </h2>
             </div>
             <div class="container-fluid text-center">
                 <div class="row">
