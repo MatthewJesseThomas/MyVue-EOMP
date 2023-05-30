e9f3601f9a6316e1770d73fd8e0b03a947e443fd
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-05-30 12:31:29 +0200

    Font-Family - Activated

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 </script>
 <style>
 #app {
-  font-family: 'Avenir', 'Helvetica', 'Arial', 'sans-serif';
+  font-family: 'Slackside One', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
