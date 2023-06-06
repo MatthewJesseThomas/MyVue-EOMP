600de72f2c688c03c9a5e539f2cd22fbe1cab089
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-06-06 11:36:51 +0200

    About Styled - Updated - Code Cleaned

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="about animated-charcter">
-    <h1 class="display-6 kage animated-charcter fw-bold text-light">About Me</h1> <br>
+  <h1 class="kage animated-character fw-bold text-light">About Me</h1>
+  <div class="about animated-character">
     <div class="col-sm-4-4 text-white text-shadow-white rounded-3 py-5 bible align-self-center wrapper">
-      <h1 class="display-6 text-decoration-underline kage animated-charcter fw-bold typing-demo">Who Is Matthew:</h1> <br>
-      <p class="lead display-6 kage animate-charcter fw-semibold">I am a Young South African male, with Aspirations of many Great deals and Strive for excellence and immense proficiency amongst all else an for my work. My passions are Engineering, Gaming, Gardening Music etc. I am the Eldest of Three kids an Always try to lead by Example for my Siblings as well as Community.</p> <br>
-      <h2 class="display-6 text-decoration-underline kage animated-charcter fw-bold typing-demo">What Motivates Matthew:</h2> <br>
-      <p class="lead display-6 kage animate-charcter fw-semibold">Money is a major factor coming from a Poverty Stricken Community although that has never stopped or Detered my sense of hunger for Success or that of being Goal driven. After my Father's Death I've had to grow up very Fast and take on Responsibilities that were at the time out of my abilities but held Steadfast and Unwavered by Circumstance went for Excellence and now am and will always be motivated by this drive for the Best Life has to Offer.
-      </p>
-      <br>
-  </div>
+      <h2 class="display-6 text-decoration-underline kage animated-character fw-bold typing-demo">Who Is Matthew:</h2>
+      <p class="lead display-6 kage animated-character fw-semibold">I am a Young South African male, with Aspirations of many Great deals and Strive for excellence and immense proficiency amongst all else an for my work. My passions are Engineering, Gaming, Gardening Music etc. I am the Eldest of Three kids an Always try to lead by Example for my Siblings as well as Community.</p>
+      <h2 class="display-6 text-decoration-underline kage animated-character fw-bold typing-demo">What Motivates Matthew:</h2>
+      <p class="lead display-6 kage animated-character fw-semibold">Money is a major factor coming from a Poverty Stricken Community although that has never stopped or Detered my sense of hunger for Success or that of being Goal driven. After my Father's Death I've had to grow up very Fast and take on Responsibilities that were at the time out of my abilities but held Steadfast and Unwavered by Circumstance went for Excellence and now am and will always be motivated by this drive for the Best Life has to Offer.</p>
+    </div>
   </div>
 </template>
-<script>
 
+<script>
 export default {
 
 }
 </script>
+
 <style scoped>
-.about{
-    margin-top: 9rem;
-    }
-    @import url("https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900");
+p {
+  font-size: 2.5rem;
+}
+
+h1, h2 {
+  font-size: 5rem;
+  color: rgba(255, 255, 255, 0.8); /* White text color with opacity */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  margin-top: 3rem; /* Adjust the margin-top value to move the element down */
+}
+
+.about {
+  margin-top: 5rem;
+}
 
 .wrapper {
   height: 80%;
-  /*This part is important for centering*/
   display: grid;
   place-items: center;
 }
@@ -42,14 +50,56 @@ export default {
 
 @keyframes typing {
   from {
-    width: 0
-  }
-}
-    
-@keyframes blink {
-  50% {
-    border-color: transparent
+    width: 0;
   }
 }
 
+@keyframes blink {
+  50% {
+    border-color: transparent;
+  }
+}
+
+/* Responsive Styles */
+@media screen and (max-width: 768px) {
+  /* Adjust properties for screens up to 768px width */
+  .about {
+    margin-top: 6rem;
+  }
+  .wrapper {
+    height: auto;
+  }
+  .typing-demo {
+    font-size: 1.5em;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  /* Adjust properties for screens up to 480px width */
+  .about {
+    margin-top: 3rem;
+  }
+  .typing-demo {
+    font-size: 1.2em;
+  }
+}
+
+@media screen and (min-width: 100px) and (max-width: 2000px) {
+  /* Adjust properties for screens within the specified width range */
+  h1 {
+    font-size: 3rem;
+    margin-top: 2rem;
+  }
+  h2 {
+    font-size: 2.5rem;
+    margin-top: 2rem;
+  }
+  .about {
+    margin-top: 4rem;
+  }
+  .typing-demo {
+    font-size: 1.8em;
+  }
+}
 </style>
+
